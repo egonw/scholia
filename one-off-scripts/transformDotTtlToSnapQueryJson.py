efc824e716d7d58caacca8a291d1d2e1ec005ad8
Egon Willighagen <egon.willighagen@gmail.com>
2024-10-19 16:49:55 +0200

    First draft; doesn't include the SPARQL yet

---
+++ one-off-scripts/transformDotTtlToSnapQueryJson.py
@@ -0,0 +1,49 @@
+import os
+import glob
+from rdflib import Graph
+
+# Path to Turtle files
+ttl_files_path = '../scholia/app/templates/*ttl'
+
+# Get the list of .ttl files
+ttl_files = glob.glob(ttl_files_path)
+
+with open("scholia.json", 'w') as json_file:
+    json_file.write("{\n")
+    json_file.write("  \"domain\": \"scholia.toolforge.org\",\n")
+    json_file.write("  \"namespace\": \"named_queries\",\n")
+    json_file.write("  \"target_graph_name\": \"wikidata\",\n")
+    json_file.write("  \"queries\": [\n")
+
+    # Process each sparql file
+    for i in ttl_files:
+        # Extract the filename (fn) and create .sparql filename
+        fn = os.path.basename(i)[0:-4]  # extract name without extension
+        sparql = i[0:-4] + ".sparql"  # create .sparql filename
+
+        # Open .ttl file to write
+        g = Graph()
+        g.parse(i)
+
+        knows_query = """prefix sh: <http://www.w3.org/ns/shacl#>
+SELECT DISTINCT ?query ?sparql
+WHERE {
+    ?query sh:select | sh:ask | sh:construct ?sparql .
+}"""
+
+        qres = g.query(knows_query)
+        for row in qres:
+            json_file.write("    {\n")
+            json_file.write(f"      \"query_id\": \"{fn}--named_queries@scholia.toolforge.org\",\n")
+            json_file.write("      \"domain\": \"scholia.toolforge.org\",\n")
+            json_file.write("      \"namespace\": \"named_queries\",\n")
+            json_file.write(f"      \"name\": \"{fn}\",\n")
+            json_file.write("      \"sparql\": \"\",\n")
+            json_file.write(f"      \"url\": \"https://raw.githubusercontent.com/WDscholia/scholia/master/scholia/app/templates/{fn}.sparql\",\n")
+            json_file.write(f"      \"title\": \"{fn}\",\n")
+            json_file.write(f"      \"description\": \"{fn}\",\n")
+            json_file.write("      \"comment\": \"\",\n")
+            json_file.write("    },\n")
+
+    json_file.write("  ]\n")
+    json_file.write("}\n")
