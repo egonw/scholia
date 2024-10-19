7ee0697e3a7b5aa5ec61cd8349cf7a0202bd2905
Egon Willighagen <egon.willighagen@gmail.com>
2024-10-19 21:00:04 +0200

    Fixed the code to output the right info

--- one-off-scripts/transformDotTtlToSnapQueryJson.py
+++ one-off-scripts/transformDotTtlToSnapQueryJson.py
@@ -23,7 +23,6 @@ with open("scholia.json", 'w') as json_file:
         fn = os.path.basename(i)[0:-4]  # extract name without extension
         sparql = i[0:-4] + ".sparql"  # create .sparql filename
 
-        print(f"Parsing {fn}")
         g.parse(i)
 
     knows_query = """prefix sh: <http://www.w3.org/ns/shacl#>
@@ -39,23 +38,24 @@ ORDER BY ASC(?query)
 
     qres = g.query(knows_query)
     for row in qres:
+        queryID = row.query.replace("https://github.com/WDScholia/scholia/scholia/app/templates/", "")
         # prepare the SPARQL
         sparqlVal = row.sparql.replace("\\", "\\\\").replace("\n", "\\n").replace("\"", "\\\"")
 
         # output the results for this SPARQL
         json_file.write("    {\n")
-        json_file.write(f"      \"query_id\": \"{fn}--named_queries@scholia.toolforge.org\",\n")
+        json_file.write(f"      \"query_id\": \"{queryID}--named_queries@scholia.toolforge.org\",\n")
         json_file.write("      \"domain\": \"scholia.toolforge.org\",\n")
         json_file.write("      \"namespace\": \"named_queries\",\n")
-        json_file.write(f"      \"name\": \"{fn}\",\n")
+        json_file.write(f"      \"name\": \"{queryID}\",\n")
         json_file.write(f"      \"sparql\": \"{sparqlVal}\",\n")
-        json_file.write(f"      \"url\": \"https://raw.githubusercontent.com/WDscholia/scholia/master/scholia/app/templates/{fn}.sparql\",\n")
+        json_file.write(f"      \"url\": \"https://raw.githubusercontent.com/WDscholia/scholia/master/scholia/app/templates/{queryID}.sparql\",\n")
         if (row.title):
             json_file.write(f"      \"title\": \"{row.title}\",\n")
         else:
-            json_file.write(f"      \"title\": \"{fn}\",\n")
-        json_file.write(f"      \"description\": \"{fn}\",\n")
-        json_file.write("      \"comment\": \"\",\n")
+            json_file.write(f"      \"title\": \"{queryID}\",\n")
+        json_file.write(f"      \"description\": \"{queryID}\",\n")
+        json_file.write("      \"comment\": \"\"\n")
         json_file.write("    },\n")
 
     json_file.write("  ]\n")
