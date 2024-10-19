c38e753a743f7b383721c15e40dad4403d482a62
Egon Willighagen <egon.willighagen@gmail.com>
2024-10-19 19:18:39 +0200

    Parse all Turtle once

--- one-off-scripts/transformDotTtlToSnapQueryJson.py
+++ one-off-scripts/transformDotTtlToSnapQueryJson.py
@@ -15,45 +15,48 @@ with open("scholia.json", 'w') as json_file:
     json_file.write("  \"target_graph_name\": \"wikidata\",\n")
     json_file.write("  \"queries\": [\n")
 
+    g = Graph()
+
     # Process each sparql file
     for i in ttl_files:
         # Extract the filename (fn) and create .sparql filename
         fn = os.path.basename(i)[0:-4]  # extract name without extension
         sparql = i[0:-4] + ".sparql"  # create .sparql filename
 
-        # Open .ttl file to write
-        g = Graph()
+        print(f"Parsing {fn}")
         g.parse(i)
 
-        knows_query = """prefix sh: <http://www.w3.org/ns/shacl#>
+    knows_query = """prefix sh: <http://www.w3.org/ns/shacl#>
 prefix dcterms: <http://purl.org/dc/terms/>
 
 SELECT DISTINCT ?query ?title ?sparql
 WHERE {
     ?query sh:select | sh:ask | sh:construct ?sparql .
     OPTIONAL { ?query dcterms:title ?title }
-}"""
+}
+ORDER BY ASC(?query)
+"""
 
-        qres = g.query(knows_query)
-        for row in qres:
-            # prepare the SPARQL
-            sparqlVal = row.sparql.replace("\\", "\\\\").replace("\n", "\\n").replace("\"", "\\\"")
+    qres = g.query(knows_query)
+    for row in qres:
+        # prepare the SPARQL
+        sparqlVal = row.sparql.replace("\\", "\\\\").replace("\n", "\\n").replace("\"", "\\\"")
 
-            # output the results for this SPARQL
-            json_file.write("    {\n")
-            json_file.write(f"      \"query_id\": \"{fn}--named_queries@scholia.toolforge.org\",\n")
-            json_file.write("      \"domain\": \"scholia.toolforge.org\",\n")
-            json_file.write("      \"namespace\": \"named_queries\",\n")
-            json_file.write(f"      \"name\": \"{fn}\",\n")
-            json_file.write(f"      \"sparql\": \"{sparqlVal}\",\n")
-            json_file.write(f"      \"url\": \"https://raw.githubusercontent.com/WDscholia/scholia/master/scholia/app/templates/{fn}.sparql\",\n")
-            if (row.title):
-                json_file.write(f"      \"title\": \"{row.title}\",\n")
-            else:
-                json_file.write(f"      \"title\": \"{fn}\",\n")
-            json_file.write(f"      \"description\": \"{fn}\",\n")
-            json_file.write("      \"comment\": \"\",\n")
-            json_file.write("    },\n")
+        # output the results for this SPARQL
+        json_file.write("    {\n")
+        json_file.write(f"      \"query_id\": \"{fn}--named_queries@scholia.toolforge.org\",\n")
+        json_file.write("      \"domain\": \"scholia.toolforge.org\",\n")
+        json_file.write("      \"namespace\": \"named_queries\",\n")
+        json_file.write(f"      \"name\": \"{fn}\",\n")
+        json_file.write(f"      \"sparql\": \"{sparqlVal}\",\n")
+        json_file.write(f"      \"url\": \"https://raw.githubusercontent.com/WDscholia/scholia/master/scholia/app/templates/{fn}.sparql\",\n")
+        if (row.title):
+            json_file.write(f"      \"title\": \"{row.title}\",\n")
+        else:
+            json_file.write(f"      \"title\": \"{fn}\",\n")
+        json_file.write(f"      \"description\": \"{fn}\",\n")
+        json_file.write("      \"comment\": \"\",\n")
+        json_file.write("    },\n")
 
     json_file.write("  ]\n")
     json_file.write("}\n")
